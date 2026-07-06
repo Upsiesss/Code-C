cdafc27b3d4e7253058f4f78db3c6890e6ff6e96
Upsiesss <144931200+Upsiesss@users.noreply.github.com>
2026-07-06 22:40:47 +0530

    Add files via upload

---
+++ nums_1to300.c
@@ -0,0 +1,16 @@
+/*Program to print numbers from 1 to 300*/
+/*Author:Mugdha         Date:06-07-26*/
+
+#include <stdio.h>
+
+int main()
+{
+    int i=1;
+    while(i<=300)
+    {
+        printf("%d\n",i);
+        i++;
+    
+    }
+    return 0;
+}
